36e4d17402c1033383943d44f2892e8695fcbbe2
kecha <kechadifarid10@gmail.com>
2024-11-13 11:22:58 +0100

    Ajout des implimentation des interface.

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -9,6 +9,7 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory {
     @Override
     public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
         // Créer un Pokedex via la fabrique
+
         IPokedex pokedex = pokedexFactory.createPokedex(new PokemonMetadataProvider(), new PokemonFactory());
 
         // Créer un entraîneur avec le nom, l'équipe et le pokédex
